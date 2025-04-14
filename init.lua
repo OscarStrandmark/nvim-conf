7a072bd64b549b62239ea07d1913f8ae2d6a120e
Oscar Strandmark <oscar.strandmark1@gmail.com>
2025-04-14 22:15:01 +0200

    Add nvim-tree keybind

--- init.lua
+++ init.lua
@@ -8,6 +8,17 @@ require("nvim-tree").setup({
 			width = 30
 		},
 })
+
+
+
+
+
+
+
+-- Keymaps
+
+vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', { desc = "Toggle file-tree" })
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
