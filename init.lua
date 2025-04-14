2a7503f4227c068ef5e613f54d13720fe3e3e79f
Oscar Strandmark <oscar.strandmark1@gmail.com>
2025-04-14 22:12:23 +0200

    add nvim-tree

--- init.lua
+++ init.lua
@@ -1,5 +1,13 @@
 require("config.lazy")
 
+require("nvim-tree").setup({
+		sort = { 
+			sorter = "case_sensitive" 
+		},
+		view = {
+			width = 30
+		},
+})
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
