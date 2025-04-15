8edcd8e55d763d812601bdcb3fa545cc61b8076b
Oscar Strandmark <oscar.strandmark1@gmail.com>
2025-04-15 23:25:44 +0200

    configure telescoe for larger projects

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,22 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+	require("telescope").setup({
+		defaults = {
+			layout_config = {
+				vertical = {
+					width = 0.95
+				},
+				horizontal = {
+					width = 0.95
+				},
+			},
+			path_display = {
+				"truncate"
+			}
+		}
+	})
+
+  end
 }
