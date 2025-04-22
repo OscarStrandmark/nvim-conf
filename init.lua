4448b33097408f620ce240ae3fae11f7688c51bf
Oscar Strandmark <oscar.strandmark@netset.com>
2025-04-22 13:59:56 +0200

    move keymaps from init to separate file

--- init.lua
+++ init.lua
@@ -14,12 +14,4 @@ vim.opt.termguicolors = true
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
--- Keymaps
-
-vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', { desc = "Toggle file-tree" })
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+require('keymaps')
---
+++ keymaps.lua
@@ -0,0 +1,11 @@
+
+--Tree
+vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', { desc = "Toggle file-tree" })
+
+
+--Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
