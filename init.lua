0da0deed3f944b4e9d587255a6b05c53a55b4ba4
Oscar Strandmark <oscar.strandmark@netset.com>
2026-04-03 20:38:34 +0200

    Full reset for 0.12

--- init.lua
+++ init.lua
@@ -1,16 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("config.lazy")
-
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-
-require("notify").setup({
-    background_colour = "#000000",
-    merge_duplicates = true
-})
 
 vim.cmd("set number")
--- lua/plugins/ui/flash.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {},
-  keys = {
-    { "j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "J", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  },
-}
