f778797f42b4e90004af1b1b5820590c36df73af
Oscar Strandmark <oscar.strandmark@netset.com>
2026-05-20 09:41:20 +0200

    update flash bindings & add markdown

--- lua/plugins/ui/flash.lua
+++ lua/plugins/ui/flash.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   opts = {},
   keys = {
-    { "j", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "J", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "l", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "L", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
   },
 }
